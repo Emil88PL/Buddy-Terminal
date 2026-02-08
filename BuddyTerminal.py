8564f55f4a313a6497504754cc61df3c6bbf9f5b
Emil <emil88pl@gmail.com>
2026-02-08 10:51:24 +0000

    delete unused import

--- BuddyTerminal.py
+++ BuddyTerminal.py
@@ -10,7 +10,6 @@ from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer, DataTable, Static, Input
 from textual.containers import Horizontal, Vertical
 from textual.screen import ModalScreen
-from textual.worker import Worker, WorkerState
 
 
 WEEK_DAYS = [
